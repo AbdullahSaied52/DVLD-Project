c5f1819b4a4f90b97c28dc383417de652783686b
AbdullahSaied52 <a9407602@gmail.com>
2026-08-15 17:54:41 +0300

    Add DTOUsers project with ClsUser class

---
+++ DTOUsers/Class1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOUsers
+{
+    public class ClsUser
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string password { get; set; }
+        public bool active { get; set; }
+        public int personid { get; set; }
+
+        public ClsUser()
+        {
+            id = -1;
+            name = "";
+            password = "";
+            active = false;
+            personid = -1;
+        }
+        public ClsUser (int id, int personid, string name, string pass, bool active)
+        {
+            this.id = id;
+            this.name = name;
+            this.password = pass;
+            this.active = active;
+            this.personid = personid;
+        }
+    }
+}
---
+++ DTOUsers/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DTOUsers")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DTOUsers")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0058f63f-bd61-471c-bfa5-68b0da4b2f6f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
