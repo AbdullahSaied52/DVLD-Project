a26d80163e2db14b5c9252884f46155873a5153d
AbdullahSaied52 <a9407602@gmail.com>
2026-08-15 19:32:53 +0300

    add CRUD operations for users in data and bussines layers

---
+++ Bussiness_Layer/ClsBussinessUser.cs
@@ -0,0 +1,49 @@
+﻿using Data_Layer;
+using DTOUsers_namespace;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bussiness_Layer
+{
+    public class ClsBussinessUser
+    {
+        public static List<DTOUser> list_user()
+        {
+            return ClsDataUser.list();
+        }
+
+        public static void add_user(DTOUser u)
+        {
+            ClsDataUser.add_user(u);
+        }
+
+        public static void update_user(DTOUser u)
+        {
+            ClsDataUser.update_user(u);
+        }
+
+        public static DTOUser get_user_ByID(int id)
+        {
+            return ClsDataUser.get_user_by_id(id);
+        }
+
+        public static bool if_user_exists(int id)
+        {
+            return ClsDataUser.if_user_exists(id);
+        }
+
+        public static void delete_user(int id)
+        {
+            ClsDataUser.delete_user(id);
+        }
+
+        public static DTOUser login(string name)
+        {
+            return ClsDataUser.get_user_by_username(name);
+        }
+    }
+
+}
---
+++ Data_Layer/ClsDataUser.cs
@@ -0,0 +1,178 @@
+﻿using DTOUsers_namespace;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Layer
+{
+    public class ClsDataUser
+    {
+        public static string connection_string = "Server=localhost;Database=DVLD;Integrated Security=True;TrustServerCertificate=True";
+
+        public static List<DTOUser> list()
+        {
+            List<DTOUser> list1 = new List<DTOUser>();
+            using (SqlConnection cnct = new SqlConnection(connection_string))
+            {
+                using (SqlCommand cmd = new SqlCommand("sp_list_users", cnct))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cnct.Open();
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            list1.Add(new DTOUser(
+                                    reader.GetInt32(reader.GetOrdinal("UserID")),
+                                    reader.GetInt32(reader.GetOrdinal("PersonID")),
+                                    reader.GetString(reader.GetOrdinal("UserName")),
+                                    reader.GetString(reader.GetOrdinal("Password")),
+                                    reader.GetBoolean(reader.GetOrdinal("IsActive"))
+                                     ));
+                        }
+                    }
+                }
+            }
+            return list1;
+        }
+
+        public static int add_user(DTOUser u)
+        {
+            using (SqlConnection cnct = new SqlConnection(connection_string))
+            {
+                using (SqlCommand cmd = new SqlCommand("sp_Add_User", cnct))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@name", u.name);
+                    cmd.Parameters.AddWithValue("@pass", u.password);
+                    cmd.Parameters.AddWithValue("@active", u.active);
+                    cmd.Parameters.AddWithValue("@personid", u.personid);
+                    SqlParameter outputID = new SqlParameter("@id", SqlDbType.Int)
+                    {
+                        Direction = ParameterDirection.Output
+                    };
+                    cmd.Parameters.Add(outputID);
+                    cnct.Open();
+                    object result = cmd.ExecuteNonQuery();
+                    if (result != null && int.TryParse(result.ToString(), out int insertedID))
+                    {
+                        return insertedID;
+                    }
+                    else
+                        return -1;
+                }
+            }
+        }
+
+        public static bool update_user(DTOUser u)
+        {
+            using (SqlConnection cnct = new SqlConnection(connection_string))
+            {
+                using (SqlCommand cmd = new SqlCommand("sp_Update_User", cnct))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@name", u.name);
+                    cmd.Parameters.AddWithValue("@pass", u.password);
+                    cmd.Parameters.AddWithValue("@active", u.active);
+                    cmd.Parameters.AddWithValue("@id", u.id);
+                    cnct.Open();
+                    return cmd.ExecuteNonQuery() == 1 ? true : false;
+                }
+            }
+        }
+
+        public static DTOUser get_user_by_id(int id)
+        {
+            using (SqlConnection cnct = new SqlConnection(connection_string))
+            {
+                using (SqlCommand cmd = new SqlCommand("sp_Get_User_By_ID", cnct))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cnct.Open();
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            return new DTOUser
+                                     (
+                                    reader.GetInt32(reader.GetOrdinal("UserID")),
+                                    reader.GetInt32(reader.GetOrdinal("PersonID")),
+                                    reader.GetString(reader.GetOrdinal("UserName")),
+                                    reader.GetString(reader.GetOrdinal("Password")),
+                                    reader.GetBoolean(reader.GetOrdinal("IsActive"))
+                                     );
+                        }
+                        else
+                            return null;
+                    }
+                }
+            }
+        }
+
+        public static bool if_user_exists(int id)
+        {
+            int result;
+            using (SqlConnection cnct = new SqlConnection(connection_string))
+            {
+                using (SqlCommand cmd = new SqlCommand("select dbo.if_user_exist(@id)", cnct))
+                {
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cnct.Open();
+                    result = (int)cmd.ExecuteScalar();
+
+                }
+            }
+            return result == 1;
+        }
+
+        public static void delete_user(int id)
+        {
+            using (SqlConnection cnct = new SqlConnection(connection_string))
+            {
+                using (SqlCommand cmd = new SqlCommand("sp_Delete_User", cnct))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public static DTOUser get_user_by_username(string username)
+        {
+            using (SqlConnection cnct = new SqlConnection(connection_string))
+            {
+                using (SqlCommand cmd = new SqlCommand("sp_Get_User_By_userName", cnct))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@name", username);
+                    cnct.Open();
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            return new DTOUser
+                                     (
+                                    reader.GetInt32(reader.GetOrdinal("UserID")),
+                                    reader.GetInt32(reader.GetOrdinal("PersonID")),
+                                    reader.GetString(reader.GetOrdinal("UserName")),
+                                    reader.GetString(reader.GetOrdinal("Password")),
+                                    reader.GetBoolean(reader.GetOrdinal("IsActive"))
+                                     );
+                        }
+                        else
+                            return null;
+                    }
+                }
+            }
+
+        }
+    }
+
+}
