9775a2d5cb2657c2f1d40240146e922259e4dfe1
AbdullahSaied52 <a9407602@gmail.com>
2026-08-15 18:16:06 +0300

    Add Bussiness_Layer and Data_Layer projects to solution

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DVLD.People;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,7 +20,8 @@ namespace DVLD
 
         private void peopleToolStripMenuItem1_Click(object sender, EventArgs e)
         {
-
+            FrmListPeople frm = new FrmListPeople();
+            frm.Show();
         }
     }
 }
