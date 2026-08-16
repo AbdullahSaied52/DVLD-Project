e4a26c5cb4927244ff1a9ab6657ba59a6933ff6a
AbdullahSaied52 <a9407602@gmail.com>
2026-08-16 15:40:23 +0300

    Add FrmListUsers form for user management

---
+++ Users/FrmListUsers.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace DVLD.Users
+{
+    partial class FrmListUsers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnaddnew = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 197);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 241);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // btnaddnew
+            // 
+            this.btnaddnew.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnaddnew.Location = new System.Drawing.Point(653, 143);
+            this.btnaddnew.Name = "btnaddnew";
+            this.btnaddnew.Size = new System.Drawing.Size(135, 48);
+            this.btnaddnew.TabIndex = 1;
+            this.btnaddnew.Text = "Add New";
+            this.btnaddnew.UseVisualStyleBackColor = true;
+            this.btnaddnew.Click += new System.EventHandler(this.btnaddnew_Click);
+            // 
+            // FrmListUsers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnaddnew);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "FrmListUsers";
+            this.Text = "FrmListUsers";
+            this.Load += new System.EventHandler(this.FrmListUsers_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnaddnew;
+    }
+}
---
+++ Users/FrmListUsers.cs
@@ -0,0 +1,31 @@
+﻿using Bussiness_Layer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DVLD.Users
+{
+    public partial class FrmListUsers : Form
+    {
+        public FrmListUsers()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmListUsers_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = ClsBussinessUser.list_user();
+        }
+
+        private void btnaddnew_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
