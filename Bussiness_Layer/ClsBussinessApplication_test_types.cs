9de506b7c28a6a15978e21a77b886ab7349b5e76
AbdullahSaied52 <a9407602@gmail.com>
2026-08-17 12:37:02 +0300

    Add ClsBussinessApplication_test_types class and references

---
+++ Bussiness_Layer/ClsBussinessApplication_test_types.cs
@@ -0,0 +1,47 @@
+﻿using Data_Layer;
+using DTO_Test_types_namespace;
+using DTOApplication_types_namespace;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bussiness_Layer
+{
+    public class ClsBussinessApplication_test_types
+    {
+
+        public static List<DTOApplication_types> list_all()
+        {
+            return ClsDataApplication_Test_types.list_all_applications();
+        }
+
+        public static void update_app(DTOApplication_types app)
+        {
+            ClsDataApplication_Test_types.edit_application_type(app);
+        }
+        public static DTOApplication_types get_by_id(int id)
+        {
+            return ClsDataApplication_Test_types.get_app_by_id(id);
+        }
+        public static DTOTest_types get_test_by_id(int id)
+        {
+            return ClsDataApplication_Test_types.get_test_by_id(id);
+        }
+
+        public static List<DTOTest_types> list_all_tests()
+        {
+            return ClsDataApplication_Test_types.list_all_TestTypes();
+        }
+
+        public static void update_test(DTOTest_types app)
+        {
+            ClsDataApplication_Test_types.edit_test_type(app);
+        }
+
+
+
+    }
+
+}
