2a196f5c0e3de8d4df63640c3f457ab7a6c884ed
AbdullahSaied52 <a9407602@gmail.com>
2026-08-17 13:53:10 +0300

    Add DTOTest_types class and project configuration files

---
+++ DTO_Test_types/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO_Test_types_namespace
+{
+    public class DTOTest_types
+    {
+        public int id { get; set; }
+
+        public string title { get; set; }
+        public string description { get; set; }
+        public decimal fees { get; set; }
+
+        public DTOTest_types(int id, string title, string description, decimal fees)
+        {
+            this.fees = fees;
+            this.id = id;
+            this.title = title;
+            this.description = description;
+        }
+    }
+}
