2b007bc5896b780da7cab45eb9e4a9d9ff899e8b
AbdullahSaied52 <a9407602@gmail.com>
2026-08-17 12:38:30 +0300

    Add DTOApplication_types class and project setup files

---
+++ DTOApplication_and_test_types/DTOApplication_types.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTOApplication_types_namespace
+{
+    public class DTOApplication_types
+    {
+        public int id { get; set; }
+
+        public string title { get; set; }
+        public decimal fees { get; set; }
+        public DTOApplication_types(int id, string name, decimal fees)
+        {
+            this.fees = fees;
+            this.id = id;
+            this.title = name;
+        }
+    }
+}
