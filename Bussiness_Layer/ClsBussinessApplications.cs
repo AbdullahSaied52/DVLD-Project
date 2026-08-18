c1205ec84a1adb342ae755c258e7ecb7195726ce
AbdullahSaied52 <a9407602@gmail.com>
2026-08-19 00:29:49 +0300

    add class for applications

---
+++ Bussiness_Layer/ClsBussinessApplications.cs
@@ -0,0 +1,19 @@
+﻿using Data_Layer;
+using DTOApplication_namespace;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bussiness_Layer
+{
+    public class ClsBussinessApplications
+    {
+        public static void add_new_app(DTOApplication app)
+        {
+            ClsDataApplications.add_new_application(app);
+
+        }
+    }
+}
