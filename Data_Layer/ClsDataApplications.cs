40872396d81959e7f551a2c8862c217bb055feb8
AbdullahSaied52 <a9407602@gmail.com>
2026-08-19 15:15:32 +0300

    Add ClsDataApplications for application data management
    Added a new `ClsDataApplications` class in the `Data_Layer` namespace
    to handle database operations for managing application-related data.
    Implemented methods for adding new applications, checking for
    existing applications, and associating applications with local
    driving licenses. Introduced a static connection string for
    database connectivity.
    
    Updated `Data_Layer.csproj` to include the new class file and added
    necessary `using` directives for database operations.

---
+++ Data_Layer/ClsDataApplications.cs
@@ -0,0 +1,97 @@
+﻿using DTOApplication_namespace;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Layer
+{
+    public class ClsDataApplications
+    {
+        public static string connection_string = "Server=localhost;Database=DVLD;Integrated Security=True;TrustServerCertificate=True";
+
+        public static int add_new_application(DTOApplication app)
+        {
+            app.userinfo = ClsDataUser.get_user_by_id(app.user_id);
+            app.person = ClsDataPerson.get_person_by_id(app.person_id);
+            app.app_type = ClsDataApplication_Test_types.get_app_by_id(app.app_type_id);
+
+
+
+            using (SqlConnection cnct = new SqlConnection(connection_string))
+            {
+                using (SqlCommand cmd = new SqlCommand("sp_add_new_application", cnct))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@person_id", app.person_id);
+                    cmd.Parameters.AddWithValue("@first_date", app.date);
+                    cmd.Parameters.AddWithValue("@last_date", DateTime.Now.ToString());
+                    cmd.Parameters.AddWithValue("@app_type_id", app.app_type_id);
+                    cmd.Parameters.AddWithValue("@app_status", app.app_status);
+                    cmd.Parameters.AddWithValue("@fees", app.fees_for_app);
+                    cmd.Parameters.AddWithValue("@userid", app.user_id);
+                    SqlParameter outputID = new SqlParameter("@app_id", SqlDbType.Int)
+                    {
+                        Direction = ParameterDirection.Output
+                    };
+                    cmd.Parameters.Add(outputID);
+                    cnct.Open();
+                    object result = cmd.ExecuteNonQuery();
+                    app.app_id = (int)outputID.Value;  // to update value of app_id immediatly
+                    return app.app_id;
+                }
+            }
+
+
+        }
+
+        public static int if_application_exist(DTOApplication app)
+        {
+            using (SqlConnection cnct = new SqlConnection(connection_string))
+            {
+                string query = @"if exists(select 1 as result from Applications 
+                                        where ApplicantPersonID=@person_id
+                                        and ApplicationTypeID=@app_type_id and ApplicationStatus=1)
+                                        begin 
+                                        select 1 as found
+                                        end
+                                        else
+                                        begin
+                                        select 0 as found
+                                        end ";
+                using (SqlCommand cmd = new SqlCommand(query, cnct))
+                {
+                    cmd.Parameters.AddWithValue("@app_type_id", app.app_type_id);
+                    cmd.Parameters.AddWithValue("@person_id", app.person_id);
+                    cnct.Open();
+                    var output =cmd.ExecuteScalar();
+                    return (int) output;
+                }
+
+            }
+        }
+        public static void add_new_localdrivinglicense(int app_id,int license_id)
+        {
+            using(SqlConnection cnct=new SqlConnection(connection_string))
+            {
+                string query = @"insert into LocalDrivingLicenseApplications(ApplicationID,LicenseClassID)
+                                values (@app_id,@license_id) ";
+                using (SqlCommand cmd = new SqlCommand(query,cnct))
+                {
+                    cmd.Parameters.AddWithValue("@app_id", app_id);
+                    cmd.Parameters.AddWithValue("@license_id", license_id);
+                    cnct.Open();
+                    cmd.ExecuteNonQuery();
+                }
+
+            }
+
+        }
+
+
+        
+    }
+}
