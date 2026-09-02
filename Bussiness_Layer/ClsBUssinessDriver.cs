890f7d5c34e5efdfdba7649bb42773d23d482482
AbdullahSaied52 <a9407602@gmail.com>
2026-09-02 19:56:34 +0300

    Add ClsBUssinessDriver class for driver management
    A new file, `ClsBUssinessDriver.cs`, has been added to the `Bussiness_Layer.csproj` project. This file introduces the `ClsBUssinessDriver` class, which is responsible for managing driver-related data.
    
    The class includes the following:
    - Properties: `driver_id`, `person_id`, `created_by_user_id`, and `date`.
    - Two constructors: a parameterless constructor with default values and a parameterized constructor for initialization.
    - A method `add_new_driver` to add a new driver record using the `ClsDataDriver.add_new_driver` method.
    
    The class uses namespaces such as `Data_Layer`, `DTOPerson_namespace`, and standard .NET libraries.

---
+++ Bussiness_Layer/ClsBUssinessDriver.cs
@@ -0,0 +1,44 @@
+﻿using Data_Layer;
+using DTOPerson_namespace;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bussiness_Layer
+{
+    internal class ClsBUssinessDriver
+    {
+        public int driver_id { get; set; }
+
+        public int person_id { get; set; }
+
+        public int created_by_user_id { get; set; }
+        public DateTime date { get; set; }
+
+        ClsBUssinessDriver()
+        {
+            this.person_id = -1;
+            this.date = DateTime.Now;
+            this.created_by_user_id = -1;
+            this.driver_id = -1;
+        }
+
+        ClsBUssinessDriver(int driverID,int personID,int UserID,DateTime date)
+        {
+            this.person_id = personID;
+            this.date = date;
+            this.created_by_user_id = UserID;
+            this.driver_id = driverID;
+        }
+
+        public void add_new_driver()
+        {
+            this.driver_id=ClsDataDriver.add_new_driver(this.person_id, this.created_by_user_id, this.date);
+        }
+
+    }
+
+    
+}
