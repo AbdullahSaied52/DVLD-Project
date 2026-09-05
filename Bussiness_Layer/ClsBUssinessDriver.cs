3ca6928b933039c5491ab9267f05c7373e7ee0c8
AbdullahSaied52 <a9407602@gmail.com>
2026-09-05 15:16:56 +0300

    Add method to fetch all drivers and update namespace
    Added a `using` directive for `System.Data` to support `DataTable`.
    Introduced a static method `get_all_drivers()` in the `ClsBUssinessDriver`
    class to retrieve all drivers via `ClsDataDriver.get_all_drivers()`.
    Ensured the `namespace` declaration aligns with `Bussiness_Layer`.

--- Bussiness_Layer/ClsBUssinessDriver.cs
+++ Bussiness_Layer/ClsBUssinessDriver.cs
@@ -2,6 +2,7 @@
 using DTOPerson_namespace;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -36,6 +37,11 @@ namespace Bussiness_Layer
         public void add_new_driver()
         {
             this.driver_id=ClsDataDriver.add_new_driver(this.person_id, this.created_by_user_id, this.date);
+        }
+
+        public static DataTable get_all_drivers()
+        {
+            return ClsDataDriver.get_all_drivers();
         }
 
     }
